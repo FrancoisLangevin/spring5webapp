31eecbb70ff018ae2a260b2d6791f21d6475773d
Francois Langevin <francois.langevin.qc@gmail.com>
2021-02-19 17:46:26 -0500

    18 - Add relationship to publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -27,28 +27,39 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher pub1 = Publisher.builder().name("NameAd1").addressLine1("Line1").city("qc").state("qc").zip("G1H1M1").build();
+        publisherRepository.save(pub1);
+
         Author eric = Author.builder().firstName("Eric").lastName("Smith").build();
         Book book1 = Book.builder().title("Domain Driveen Design").isbn("12345").build();
         eric.setBooks(new HashSet<>(Arrays.asList(book1)));
         book1.setAuthors(new HashSet<>(Arrays.asList(eric)));
 
+        book1.setPublisher(pub1);
+        pub1.setBooks(new HashSet<>(Arrays.asList(book1)));
+
         authorRepository.save(eric);
         bookRepository.save(book1);
+        publisherRepository.save(pub1);
 
         Author john = Author.builder().firstName("John").lastName("Lan").build();
         Book book2 = Book.builder().title("Old JB").isbn("12589").build();
         john.setBooks(new HashSet<>(Arrays.asList(book2)));
         book2.setAuthors(new HashSet<>(Arrays.asList(john)));
 
+        book2.setPublisher(pub1);
+        pub1.getBooks().add(book2);
+
         authorRepository.save(john);
         bookRepository.save(book2);
-
-        Publisher pub1 = Publisher.builder().name("NameAd1").addressLine1("Line1").city("qc").state("qc").zip("G1H1M1").build();
         publisherRepository.save(pub1);
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Publisher number of bokkes: " + pub1.getBooks().size());
 
 
     }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -2,10 +2,9 @@ package guru.springframework.spring5webapp.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,6 +25,9 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
 
     @Override
     public String toString() {
