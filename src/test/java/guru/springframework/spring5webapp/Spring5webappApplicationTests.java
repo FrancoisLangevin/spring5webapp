317efecd3282b11733f709e3f164a61af016007b
Francois Langevin <francois.langevin.qc@gmail.com>
2021-02-22 08:53:43 -0500

    Change release et fixe test

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWit(SpringRunner.class)
 @SpringBootTest
 public class Spring5webappApplicationTests {
 
